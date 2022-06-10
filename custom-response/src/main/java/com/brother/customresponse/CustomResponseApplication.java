43e8bb5985fdf4fbc219f478ee00e226d349d743
rakibccj <rakibccj@gmail.com>
2022-06-10 17:53:09 +0600

    Initiate project
    -- Project initiate with dependency of the web, lombok

---
+++ custom-response/src/main/java/com/brother/customresponse/CustomResponseApplication.java
@@ -0,0 +1,13 @@
+package com.brother.customresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomResponseApplication.class, args);
+    }
+
+}

eca71677edad3238826401f91d5716fb107fbe7a
rakib <rakibccj@gmail.com>
2022-06-20 23:02:03 +0600

    -- Sending Mail By AWS SES

---
+++ aws/aws-ses/src/test/java/com/brother/awsses/AwsSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brother.awsses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

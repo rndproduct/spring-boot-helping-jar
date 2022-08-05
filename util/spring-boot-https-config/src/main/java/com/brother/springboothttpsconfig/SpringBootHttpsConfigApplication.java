a3d19049e4798b35fbe6e1fdd0b9d99707dbd9fa
rakibccj <rakibccj@gmail.com>
2022-08-05 16:15:14 +0600

    Enable HTTPS in the spring-boot application - Generate SSL self-signed certificate using Java keytool - Enable HTTPS for a Spring Boot application - Install self-signed certificate on local computer so the browser will trust the certificate

---
+++ util/spring-boot-https-config/src/main/java/com/brother/springboothttpsconfig/SpringBootHttpsConfigApplication.java
@@ -0,0 +1,13 @@
+package com.brother.springboothttpsconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHttpsConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHttpsConfigApplication.class, args);
+    }
+
+}
